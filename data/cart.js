0aee1bd5c2824e020923c47938f937cac7dc9b13
Abhay Negi <negiabhay2003@gmail.com>
2025-08-11 17:25:07 +0530

    move order summary into its onw file same for payment summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,13 @@
-export let cart= JSON.parse(localStorage.getItem('cart')) || [];
+export let cart= JSON.parse(localStorage.getItem('cart')) || [{
+    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity:2,
+    deliveryOptionId:'1'
+},{
+    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity:1,
+    deliveryOptionId:'2'
+        
+}];
 
 export function saveToStorage(){
     localStorage.setItem('cart',JSON.stringify(cart));
@@ -57,3 +66,13 @@ export function removeFromCart(productId){
     saveToStorage();
 }
 
+export function updateDeliveryOption(productId, deliveryOptionId){
+    let matchingItem;
+    cart.forEach((cartItem)=>{
+        if(productId===cartItem.productId){
+            matchingItem=cartItem;
+        }
+    });
+    matchingItem.deliveryOptionId=deliveryOptionId//right side is the new delivery optioin id
+    saveToStorage();
+}
