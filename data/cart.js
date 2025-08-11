62bc9b12f1b023c7af811124de53887a27ef7c56
Abhay Negi <negiabhay2003@gmail.com>
2025-08-11 15:59:27 +0530

    save data and generate html for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,11 @@
-export let cart=[{
-    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
-},{
-    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity:1
-},{
-    productId:'83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
-    quantity:1
-}];
+export let cart= JSON.parse(localStorage.getItem('cart')) || [];
+
+
+
+
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addToCart(productId){
         let matchingItem;
@@ -38,6 +36,7 @@ export function addToCart(productId){
             });
             
         }
+        saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -49,5 +48,6 @@ export function removeFromCart(productId){
 
     });
     cart=newCart;
+    saveToStorage();
 }
 
